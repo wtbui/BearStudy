a1d46c65a38fed6d54d6cb0112044491d5a25c96
wtbui <bui.william30@gmail.com>
2023-04-30 03:41:13 -0700

    Update ambience.js

--- bear-study-react/src/components/ambience.js
+++ bear-study-react/src/components/ambience.js
@@ -4,19 +4,19 @@ function ambience() {
 
 }
 
-function slider() {
-    const [value, setValue] = useState(50);
+// function slider() {
+//     const [value, setValue] = useState(50);
 
-    const handleChange = (event) => {
-        setValue(event.target.value);
-    }
+//     const handleChange = (event) => {
+//         setValue(event.target.value);
+//     }
 
-    return(
-        <div>
-            <input type="range" min="0" max="100" value={value} onChange={handleChange} />
-            <p>{value}</p>
-        </div>
-    );
-} 
+//     return(
+//         <div>
+//             <input type="range" min="0" max="100" value={value} onChange={handleChange} />
+//             <p>{value}</p>
+//         </div>
+//     );
+// } 
 
 export default ambience;
