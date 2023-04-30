58fa24a782ab9fcbad3b486543601821fa4cd74c
wtbui <bui.william30@gmail.com>
2023-04-30 03:35:09 -0700

    icons

--- bear-study-react/src/components/slider.js
+++
@@ -1,9 +0,0 @@
-import ReactSlider from "react-slider";
-
-const Slider = () => {
-    return (
-    <ReactSlider />
-    );
-};
-
-export default Slider;
--- bear-study-react/src/main.js
+++ bear-study-react/src/main.js
@@ -1,15 +1,39 @@
 import { Link } from "react-router-dom"
-import "./styles/main.css" 
+import "./styles/main.css"
+import doeIcon from './assets/doe_icon.png';
+import gladeIcon from './assets/glade_icon.png';
+import moffittIcon from './assets/moffit_icon.png';
+
 
 export default function Main() {
-    return (
-        <div className="main">
-            This is the main page!
-            <div>
-                <Link to="./pages/moffit">Link to Moffit</Link>
-            </div>
+   return (
+       <div>
+       <div className="bear">
+           bear study
+       </div>
 
-            example text
-        </div>
-    )
+               <div className="doe">
+                   <Link to="./">
+                   <img src={doeIcon} alt="Icon of Doe" />
+                   <div class="doetext">Doe</div>
+                   </Link>
+             </div>
+
+
+               <div className="glade">
+                    <Link to="./">
+                    <img src={gladeIcon} alt="Icon of Glade"/>
+                    <div class="gladetext">Memorial Glade</div>
+                    </Link>
+               </div>
+
+
+                <div className="moffitt">
+                   <Link to="./pages/moffit">
+                       <img src={moffittIcon} alt="Icon of Moffitt"/>
+                       <div class="moffitttext">Moffitt</div>
+                   </Link>
+                </div>
+    </div>
+   )
 }
