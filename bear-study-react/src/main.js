78c7b3c9b2dd415b067950d013d2f152144d8b49
10084z <127453863+10084z@users.noreply.github.com>
2023-05-01 17:46:26 -0700

    neww!!!!!!
    update!!!!!!

--- bear-study-react/src/main.js
+++ bear-study-react/src/main.js
@@ -9,7 +9,7 @@ export default function Main() {
    return (
        <div>
        <div className="bear">
-           bear study
+           <div className="bstext">bear study</div>
        </div>
 
                <div className="doe">
@@ -37,3 +37,18 @@ export default function Main() {
     </div>
    )
 }
+
+// const handleOnMouseMove = e => {
+//     const { currentTarget: target} = e;
+
+//     const rect = target.getBoundingClientRect(),
+//     x = e.clientX - rect.left,
+//     y = e.clientY - rect.top;
+
+//     target.style.setProperty("--mouse-x", '${x}px');
+//     target.style.setProperty("--mouse-y", '${y}px');
+// }
+
+// for(const bear of document.querySelectorAll(".bear")){
+//     bear.onmousemove = e => handleOnMouseMove(e);
+// }
